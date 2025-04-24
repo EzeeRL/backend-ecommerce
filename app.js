ff4fda1afb3da20b6b91dc26103569fe49527d18
JuanManuelRamosL <juanmanuelramoslencina@gmail.com>
2025-04-24 14:48:29 -0300

    cors

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
 const productsRoutes = require("./routes/zapatillasRoutes");
 const camperasRoutes = require("./routes/camperasRoutes");
 
+app.use(cors());
 app.use(express.json());
 app.use("/Zapatillas", productsRoutes);
 app.use("/Camperas", camperasRoutes);
